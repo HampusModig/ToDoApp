fd7f524a181475551a2ffa23b84d60d1184baad3
Noshin Shukri <noshinshukri@gmail.com>
2026-02-12 12:37:05 +0100

    Skapat filter för gjorda och ogjorda uppgifter och ändrar menyval till filtrering istället

--- ToDoApp/TaskManager.cs
+++ ToDoApp/TaskManager.cs
@@ -22,5 +22,23 @@ namespace ToDoApp
                 Console.WriteLine($"ID: {t.Id} | Titel: {t.Title} | Klar: {(t.IsCompleted ? "Ja" : "Nej")}");
             }
         }
+
+        public void DisplayTaskByStatus(bool IsCompleted)
+        {
+            var filteredTasks = Tasks.FindAll(t => t.IsCompleted == IsCompleted).ToList();
+
+            if (filteredTasks.Count == 0)
+            {
+                Console.WriteLine("Inga uppgifter att visa.");
+                return;
+            }
+            else
+            {
+                foreach(var task in filteredTasks)
+                {
+                    Console.WriteLine($"ID: {task.Id} | Titel: {task.Title} | Klar: {(task.IsCompleted ? "Ja" : "Nej")}");
+                }
+            }
+        }
     }
 }
