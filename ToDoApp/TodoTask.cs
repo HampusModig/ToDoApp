6c7ba267b2e52d1f3c8598cf2adda2b07e2f4adb
Noshin Shukri <noshinshukri@gmail.com>
2026-02-12 09:36:30 +0100

    Skapat properties i todotask klassen

--- ToDoApp/TodoTask.cs
+++ ToDoApp/TodoTask.cs
@@ -6,5 +6,14 @@ namespace ToDoApp
 {
     internal class TodoTask
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+        public TodoTask(int id, string title)
+        {
+            Id = id;
+            Title = title;
+            IsCompleted = false;
+        }
     }
 }
