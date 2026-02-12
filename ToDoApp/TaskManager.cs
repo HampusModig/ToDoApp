4f02ae26d4bb5acc0cbd66cf691cce6db4eb4790
HampusModig <hampusmodig@gmail.com>
2026-02-12 09:48:00 +0100

    Added AddTask method

---
+++ ToDoApp/TaskManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoApp
+{
+    internal class TaskManager
+    {
+        public List<TodoTask> Tasks = new List<TodoTask>();
+        public int IdCounter = 0;
+
+        public void AddTask(string Title)
+        {
+            Tasks.Add(new TodoTask(IdCounter, Title));
+            IdCounter++;
+        }
+
+    }
+}
