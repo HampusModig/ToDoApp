38eeca6c6c5b531a50f1d4a58eebf25a21948c48
Noshin Shukri <noshinshukri@gmail.com>
2026-02-12 09:37:48 +0100

    Kommentar

--- ToDoApp/TodoTask.cs
+++ ToDoApp/TodoTask.cs
@@ -6,6 +6,7 @@ namespace ToDoApp
 {
     internal class TodoTask
     {
+
         public int Id { get; set; }
         public string Title { get; set; }
         public bool IsCompleted { get; set; }
@@ -15,5 +16,20 @@ namespace ToDoApp
             Title = title;
             IsCompleted = false;
         }
+
+        /* 
+        * 1. Skapa TodoTask-klass med properties (Id, Title, IsCompleted)
+        * 2. Skapa metod AddTask() i TaskManager-klass
+        * 3. Skapa menyval för "Lägg till uppgift"
+        * 4. Implementera input-validering (tom titel)
+        * 5. Testa funktionalitet
+✓ Användaren kan välja "Lägg till uppgift" från huvudmenyn​
+✓ Programmet ber om en titel för uppgiften​
+✓ Uppgiften läggs till i listan med ett unikt ID​
+✓ Ett bekräftelsemeddelande visas​
+✓ Tom titel ska avvisas med felmeddelande​
+✓ Nya uppgifter är automatiskt "inte klara" (IsCompleted = false)
+*/
+
     }
 }
