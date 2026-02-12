1df9ae13dffdde712f552b7445cd41fef3ba34de
HampusModig <hampusmodig@gmail.com>
2026-02-12 10:12:20 +0100

    la till måsvingar

--- ToDoApp/Program.cs
+++ ToDoApp/Program.cs
@@ -15,6 +15,7 @@ public class Program
     public static void Main()
     {
         bool running = true;
+        TaskManager taskManager = new TaskManager();
 
         while (running)
         {
@@ -26,38 +27,41 @@ public class Program
             Console.WriteLine("0. Avsluta");
             Console.Write("Välj: ");
 
-    switch (Console.ReadLine())
-    {
-        case "1":
-            // TODO US1: Lägga till uppgift
-/* 
-        * 1. Skapa TodoTask-klass med properties (Id, Title, IsCompleted)
-        * 2. Skapa metod AddTask() i TaskManager-klass
-        * 3. Skapa menyval för "Lägg till uppgift"
-        * 4. Implementera input-validering (tom titel)
-        * 5. Testa funktionalitet
-✓ Användaren kan välja "Lägg till uppgift" från huvudmenyn​
-✓ Programmet ber om en titel för uppgiften​
-✓ Uppgiften läggs till i listan med ett unikt ID​
-✓ Ett bekräftelsemeddelande visas​
-✓ Tom titel ska avvisas med felmeddelande​
-✓ Nya uppgifter är automatiskt "inte klara" (IsCompleted = false)
-*/
+            switch (Console.ReadLine())
+            {
+                case "1":
+                // TODO US1: Lägga till uppgift
+                /* 
+                        * 1. Skapa TodoTask-klass med properties (Id, Title, IsCompleted)
+                        * 2. Skapa metod AddTask() i TaskManager-klass
+                        * 3. Skapa menyval för "Lägg till uppgift"
+                        * 4. Implementera input-validering (tom titel)
+                        * 5. Testa funktionalitet
+                ✓ Användaren kan välja "Lägg till uppgift" från huvudmenyn​
+                ✓ Programmet ber om en titel för uppgiften​
+                ✓ Uppgiften läggs till i listan med ett unikt ID​
+                ✓ Ett bekräftelsemeddelande visas​
+                ✓ Tom titel ska avvisas med felmeddelande​
+                ✓ Nya uppgifter är automatiskt "inte klara" (IsCompleted = false)
+                */
 
-        case "2":
-            // TODO US2: Visa alla uppgifter
-            break;
-        case "3":
-            // TODO US3: Markera som klar
-            break;
-        case "4":
-            // TODO US4: Ta bort uppgift
-            break;
-        case "0":
-            running = false;
-            break;
-        default:
-            Console.WriteLine("Ogiltigt val, försök igen.");
-            break;
+
+                case "2":
+                    // TODO US2: Visa alla uppgifter
+                    break;
+                case "3":
+                    // TODO US3: Markera som klar
+                    break;
+                case "4":
+                    // TODO US4: Ta bort uppgift
+                    break;
+                case "0":
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Ogiltigt val, försök igen.");
+                    break;
+            }
+        }
     }
 }
